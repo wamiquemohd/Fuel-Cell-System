4e41b184f0dd3e4cf0ec99e3ee8a3c518083c284
Hossein Baghaei <hosseinb444@gmail.com>
2023-01-06 15:42:27 +0100

    Initial_MQTT

--- GUI_Pycode/main.py
+++ GUI_Pycode/main.py
@@ -15,11 +15,18 @@ from tkinter import ttk
 root = tk.Tk();
 root.title("GUI");
 root.configure(background = 'light blue')
-root.geometry("700x500") # set the window size
+root.geometry("1400x750") # set the window size
 #Global vars
 data = ["0","0","0","0"];
 data_VBAT=np.array([])
-y = 450;#Buttons location in y axis
+data_CBAT=np.array([])
+data_VFC=np.array([])
+data_CFC=np.array([])
+width_plt = (root.winfo_screenwidth()/2) - 80
+#print(width_plt)
+height_plt = (root.winfo_screenheight()/2) - 180
+#print(height_plt)
+y = 700;#Buttons location in y axis
 #host name is localhost because both broker and python are Running on same machine/Computer.
 broker="localhost";  #host name
 port=1883;
@@ -45,7 +52,10 @@ def split_data(data):
   print(x[2]);# Fuel cell voltage
   print(x[3]);# Fuel cell current
   print("Task done");
-  plot_data(float(x[0]))
+  plot_data_VBAT(float(x[0]))
+  plot_data_CBAT(float(x[1]))
+  plot_data_VFC(float(x[2]))
+  plot_data_CFC(float(x[3]))
 
 client= paho.Client("user") #create client object
 
@@ -62,7 +72,8 @@ print("Waiting for data...................")
 client.subscribe(topic)#subscribe topic test
 
 #------create plot function---------------
-def plot_data(x):
+#------Plot Battery voltage---------------
+def plot_data_VBAT(x):
     global data_VBAT
     V_BAT = float(x)/1000;
     print(V_BAT)
@@ -71,27 +82,123 @@ def plot_data(x):
     else:
             data_VBAT[0:99] = data_VBAT[1:100]
             data_VBAT[99] = float(V_BAT)
-    lines.set_xdata(np.arange(0,len(data_VBAT)))
-    lines.set_ydata(data_VBAT)
-    canvas.draw()
-    root.after(1,plot_data)
+    lines_VBAT.set_xdata(np.arange(0,len(data_VBAT)))
+    lines_VBAT.set_ydata(data_VBAT)
+    canvas_VBAT.draw()
+    root.after(1,plot_data_VBAT)
 
+    #------Plot Battery current---------------
+def plot_data_CBAT(x):
+    global data_CBAT
+    C_BAT = float(x)/1000;
+    print(C_BAT)
+    if(len(data_CBAT) < 100):
+            data_CBAT = np.append(data_CBAT,float(C_BAT))
+    else:
+            data_CBAT[0:99] = data_CBAT[1:100]
+            data_CBAT[99] = float(C_BAT)
+    lines_CBAT.set_xdata(np.arange(0,len(data_CBAT)))
+    lines_CBAT.set_ydata(data_CBAT)
+    canvas_CBAT.draw()
+    root.after(1,plot_data_CBAT)
+
+   #------Plot Fuel-cell voltage---------------
+def plot_data_VFC(x):
+    global data_VFC
+    V_FC = float(x)/1000;
+    print(V_FC)
+    if(len(data_VFC) < 100):
+            data_VFC = np.append(data_VFC,float(V_FC))
+    else:
+            data_VFC[0:99] = data_VFC[1:100]
+            data_VFC[99] = float(V_FC)
+    lines_VFC.set_xdata(np.arange(0,len(data_VFC)))
+    lines_VFC.set_ydata(data_VFC)
+    canvas_VFC.draw()
+    root.after(1,plot_data_VFC)
+
+      #------Plot Fuel-cell current---------------
+def plot_data_CFC(x):
+    global data_CFC
+    C_FC = float(x)/1000;
+    print(C_FC)
+    if(len(data_CFC) < 100):
+            data_CFC = np.append(data_CFC,float(C_FC))
+    else:
+            data_CFC[0:99] = data_CFC[1:100]
+            data_CFC[99] = float(C_FC)
+    lines_CFC.set_xdata(np.arange(0,len(data_CFC)))
+    lines_CFC.set_ydata(data_CFC)
+    canvas_CFC.draw()
+    root.after(1,plot_data_CFC)
 #------create Plot object on GUI----------
 # add figure canvas
-fig = Figure();
-ax = fig.add_subplot(111)
+#------plot for Battery voltage-----------
+fig_VBAT = Figure();
+ax_VBAT = fig_VBAT.add_subplot(111)
 
 #ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
-ax.set_title('Battery voltage');
-ax.set_xlabel('Time')
-ax.set_ylabel('Voltage')
-ax.set_xlim(0,100)
-ax.set_ylim(0,20)
-lines = ax.plot([],[])[0]
+ax_VBAT.set_title('Battery voltage');
+ax_VBAT.set_xlabel('Time')
+ax_VBAT.set_ylabel('Voltage')
+ax_VBAT.set_xlim(0,100)
+ax_VBAT.set_ylim(0,20)
+lines_VBAT = ax_VBAT.plot([],[])[0]
 
-canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
-canvas.get_tk_widget().place(x = 10,y=10, width = 500,height = 400)
-canvas.draw()
+canvas_VBAT = FigureCanvasTkAgg(fig_VBAT, master=root)  # A tk.DrawingArea.
+canvas_VBAT.get_tk_widget().place(x = 10,y=10, width = width_plt,height = height_plt)
+canvas_VBAT.draw()
+
+#------plot for Battery current-----------
+fig_CBAT = Figure();
+ax_CBAT = fig_CBAT.add_subplot(111)
+
+#ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
+ax_CBAT.set_title('Battery current');
+ax_CBAT.set_xlabel('Time')
+ax_CBAT.set_ylabel('Current')
+ax_CBAT.set_xlim(0,100)
+ax_CBAT.set_ylim(0,10)
+lines_CBAT = ax_CBAT.plot([],[])[0]
+
+canvas_CBAT = FigureCanvasTkAgg(fig_CBAT, master=root)  # A tk.DrawingArea.
+width_height_canvas_VBAT = canvas_VBAT.get_width_height()
+canvas_CBAT.get_tk_widget().place(x = width_height_canvas_VBAT[0] + (width_height_canvas_VBAT[0])/10,y=10, width = width_plt,height = height_plt)
+canvas_CBAT.draw()
+
+#------plot for Fuel-cell voltage-----------
+fig_VFC = Figure();
+ax_VFC = fig_VFC.add_subplot(111)
+
+#ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
+ax_VFC.set_title('Fuel-cell voltage');
+ax_VFC.set_xlabel('Time')
+ax_VFC.set_ylabel('Voltage')
+ax_VFC.set_xlim(0,100)
+ax_VFC.set_ylim(0,65)
+lines_VFC = ax_VFC.plot([],[])[0]
+
+canvas_VFC = FigureCanvasTkAgg(fig_VFC, master=root)  # A tk.DrawingArea.
+width_height_canvas_VBAT = canvas_VBAT.get_width_height()
+canvas_VFC.get_tk_widget().place(x = 10,y=width_height_canvas_VBAT[1] - 120, width = width_plt,height = height_plt)
+canvas_VFC.draw()
+
+#------plot for Fuel-cell current-----------
+fig_CFC = Figure();
+ax_CFC = fig_CFC.add_subplot(111)
+
+#ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
+ax_CFC.set_title('Fuel-cell current');
+ax_CFC.set_xlabel('Time')
+ax_CFC.set_ylabel('Current')
+ax_CFC.set_xlim(0,100)
+ax_CFC.set_ylim(0,20)
+lines_CFC = ax_CFC.plot([],[])[0]
+
+canvas_CFC = FigureCanvasTkAgg(fig_CFC, master=root)  # A tk.DrawingArea.
+width_height_canvas_VBAT = canvas_VBAT.get_width_height()
+canvas_CFC.get_tk_widget().place(x = width_height_canvas_VBAT[0] + (width_height_canvas_VBAT[0])/10,y=width_height_canvas_VBAT[1] - 120, width = width_plt,height = height_plt)
+canvas_CFC.draw()
 
 #----------Create Buttons-----------------
 #Buttons Function
